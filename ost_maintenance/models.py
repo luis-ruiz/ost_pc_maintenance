2e9ee0955cdb241cd3740a3f08a0dc7dad016e76
Luis Ruiz <lruiz@opensoft.opensoftech.com>
2015-04-14 14:51:01 +0000

    Added machine model with passing test

--- ost_maintenance/models.py
+++ ost_maintenance/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Machine(models.Model):
+    netbios_name = models.CharField(max_length=10)
+    workgroup = models.CharField(max_length=10)
+    create_dt = models.DateTimeField()
--- ost_maintenance/tests.py
+++ ost_maintenance/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.utils import timezone
+from ost_maintenance.models import Machine
 
 # Create your tests here.
+
+class MachineTest(TestCase):
+    def test_create_machine(self):
+        machine = Machine()
+
+        machine.netbios_name = 'ALT_LAP001'
+        machine.workgroup = 'Alterna'
+        machine.create_dt = timezone.now()
+
+        machine.save()
+
+        all_machines = Machine.objects.all()
+        self.assertEquals(len(all_machines), 1)
+        only_machine = all_machines[0]
+        self.assertEquals(only_machine, machine)
+
+        self.assertEquals(only_machine.netbios_name, 'ALT_LAP001')
+        self.assertEquals(only_machine.workgroup, 'Alterna')
+        self.assertEquals(only_machine.create_dt.day, machine.create_dt.day)
+        self.assertEquals(only_machine.create_dt.month, machine.create_dt.month)
+        self.assertEquals(only_machine.create_dt.year, machine.create_dt.year)
+        self.assertEquals(only_machine.create_dt.hour, machine.create_dt.hour)
+        self.assertEquals(only_machine.create_dt.minute, machine.create_dt.minute)
+        self.assertEquals(only_machine.create_dt.second, machine.create_dt.second)
